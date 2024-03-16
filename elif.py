02544c1e18d59d140071008236cc85d64933f488
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-03-16 19:26:10 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the value of num:"))
+if(a<0):
+    print('Number is negative')
+elif(a==0):
+    print('Number is zero')
+elif(a==999):
+    print('Number is special')
+else:
+    print('number is positive')    
