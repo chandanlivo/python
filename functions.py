4bcc2687fc7562029c24e61c4341c336a6d16422
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-03-18 19:45:30 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,36 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a, b):
+    if(a> b):
+     print("First number is greater ")
+    else:
+     print("Second number is greater ")
+
+def isLesser(a,b):
+   pass
+a = 10
+b = 11
+#if(a> b):
+#    print("First number is greater ")
+#else:
+ #   print("Second number is greater ")
+isGreater(a,b)
+
+#gmean1 = (a*b)/(a+b)
+#print(gmean1)
+calculateGmean(a, b)
+print("---------------")
+#----------------------------------------------------------------------------
+c = 8
+d = 7
+#if(c> d):
+#    print("First number is greater ")
+#else:
+#    print("Second number is greater ")
+isGreater(c,d)
+
+#gmean2 = (c*d)/(c+d)
+#print(gmean2)
+calculateGmean(c, d)
