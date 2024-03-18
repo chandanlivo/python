5d5c2aeca89c808615836a54aa64fb944c916379
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-03-18 19:44:18 +0530

    Add files via upload

---
+++ for_loops.py
@@ -0,0 +1,11 @@
+#name = 'chandan'
+#for i in name:
+#    print(i)
+#    if(i=='d'):
+#        print('this is something special')
+
+#for k in range(1,11):
+#    print(k+ 1)
+
+for k in range(0, 13, 2):  #prints from 1 to 12 with difference between each number is 2
+    print(k+1)
