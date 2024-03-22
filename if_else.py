9534a37f0d4b469342afc0239150d6925424bead
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-03-23 00:00:33 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,13 @@
+a = int(input('Enter your age\n'))
+print('Your age is:', a)
+print(a<18)
+print(a<=18)
+print(a>18)
+print(a>=18)
+print(a==18)
+ 
+if(a>18):
+    print('You can drive')
+else:
+    print("You cannot drive")    
+
