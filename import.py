1073bbe506784e7e73738d54e1d3038fd331cd10
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-03-23 00:00:51 +0530

    Add files via upload

---
+++ import.py
@@ -0,0 +1,29 @@
+# import math 
+# result = math.sqrt(9)
+# print(result)
+
+# from math import sqrt , pi 
+
+# from math import *
+# result = sqrt(9) * pi
+# print(result)
+
+# from math import pi, sqrt  as s 
+# result = s(9) * pi
+# print(result)
+
+# import math as m
+# result = m.sqrt(9) * m.pi
+# print(result)
+
+# import math as math_builtin_python
+# result = math_builtin_python.sqrt(9) * math_builtin_python.pi
+# print(result)
+
+# from chandan import welcome, chandan
+# welcome()
+# print(chandan)
+
+import chandan 
+chandan.welcome()
+# print(ch.chandan)
