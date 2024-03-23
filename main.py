7c9c88362e7a07a5d21178c70b283a95aa00d85d
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-03-24 00:18:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#hii
+print("hey \'I' \'hii' \nand this",'hiiii',sep='-',end="nnn\n") 
+a = 1
+print(a)
+b = "chandu"
+a1 = 211
+print(b)
+print(a + a1)
+print("type of a is", type(a))
+print("type of a1 is", type(a1))
+print("type of b is", type(b))
+list = [2,3,[-4.2,5],["banana","apple"]]
+tuple = [['banana','apple'],['red','green']]
+dict = {'name':'chandan'}
+print(list)
+print(tuple)
+print(dict)
