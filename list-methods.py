df340093c0d426239de37fc74b8ed0b1f05b4bd2
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-03-24 00:18:24 +0530

    Add files via upload

---
+++ list-methods.py
@@ -0,0 +1,30 @@
+l = [34, 11, 1,2,3,4,5]
+
+# l.append(6)
+# print(l)
+
+# l.sort()
+# print(l)
+
+# l.sort(reverse=True)
+# print(l)
+
+# l.reverse()
+# print(l)
+
+# print(l.index(5))
+
+ #m = l contents of l is copied into m
+ # m[0] = 9 first element of l is changed into 9
+# if you change m , l will also be changed
+# print(l)
+
+# m= l.copy()
+# m[0] = 9
+# l.insert(2, 22)
+m= [15, 25, 35]
+k= l+ m
+print(k)
+# l.extend(m )
+print(l)
+# print(m)
