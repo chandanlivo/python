92c607d8045813b0b534b1194d43ef006ffe6861
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-03-31 12:41:32 +0530

    Update emp.py

--- emp.py
+++ emp.py
@@ -8,3 +8,8 @@ class Employee:
             i = i+ 1
         return i
     
+    def __str__(self):
+        return f'The name of the employee is {self.name} str'
+    
+    def __repr__(self):
+        return f"The name of the employee is('{self.name}') repr"
