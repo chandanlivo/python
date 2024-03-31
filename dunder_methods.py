01c532ecfd9fadb5bb94960c212df3fcb3f186b7
Chandan N <138152782+chandanlivo@users.noreply.github.com>
2024-03-31 12:40:54 +0530

    Update dunder_methods.py

--- dunder_methods.py
+++ dunder_methods.py
@@ -1,5 +1,7 @@
 from emp import Employee  # here 'emp' is a file , in which 'Employee' class has been implemented
 
 e = Employee()
-print(e.name)
-print(len(e.name))
+# print(e.name)
+# print(len(e.name))
+print(str(e))
+print(repr(e))
